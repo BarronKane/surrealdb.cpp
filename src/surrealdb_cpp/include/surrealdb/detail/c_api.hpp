b71edbe4f314648a49dec5a19cf102b6cd29f756
Lance Wallis <walance@pvas.me>
2026-09-19 01:44:47 -0500

    Bump to 0.1.3

--- src/surrealdb_cpp/include/surrealdb/detail/c_api.hpp
+++ src/surrealdb_cpp/include/surrealdb/detail/c_api.hpp
@@ -53,8 +53,8 @@ extern "C" {
 // Minimum surrealdb.c
 // ---------------------------------------------------------------------------
 //
-// This library calls functions that do not exist in every release -- 0.1.2
-// needs `sr_session_fork` and `sr_runtime_init`, added in surrealdb.c 0.3.2 --
+// This library calls functions that do not exist in every release -- 0.1.3
+// needs `sr_session_fork`, `sr_runtime_init` and `sr_rpc_kill_on` --
 // and building against an older header does not fail anywhere useful. It fails
 // as an undeclared identifier partway down a header the user did not write, or,
 // if the declaration happens to exist but the symbol does not, at the link with
